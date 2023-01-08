9c79d50361c749b59a998fe101e3880e37d14c65
trustedcoder <trustedcoder11@gmail.com>
2023-01-08 06:20:42 +0100

    fetched all jokes from db into jokes route

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,9 +1,25 @@
-import { Outlet } from "@remix-run/react";
+import { Link, Outlet, useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { db } from "~/utils/db.server";
+
+export const loader = async () => {
+    return json({
+      jokeListItems: await db.joke.findMany(),
+    });
+  };
 
 export default function JokesRoute(){
+    const data = useLoaderData<typeof loader>();
     return (
         <div>
             <h1>J🤪KES</h1>
+            <ul>
+              {data.jokeListItems.map((joke) => (
+                <li key={joke.id}>
+                  <Link to={(joke.id).toString()}>{joke.name}</Link>
+                </li>
+              ))}
+            </ul>
             <main>
                 <Outlet/>
             </main>
