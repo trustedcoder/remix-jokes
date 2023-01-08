a3c043e33acb8d66c7ad7926fc1c8b3744a48b81
trustedcoder <trustedcoder11@gmail.com>
2023-01-08 11:19:40 +0100

    implemented get joke by id

--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,12 +1,32 @@
+import { Link, Outlet, useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { db } from "~/utils/db.server";
+
+export const loader = async () => {
+    const count = await db.joke.count();
+    const randomRowNumber = Math.floor(Math.random() * count);
+    const [joke] = await db.joke.findMany({
+        take: 1,
+        skip: randomRowNumber,
+      });
+    if (!joke) {
+      throw new Error("Joke not found");
+    }
+    return json({ joke });
+  };
+
 
 export default function JokesIndex(){
+    const data = useLoaderData<typeof loader>();
     return (
         <div>
             <p>Here's a random joke:</p>
             <p>
-                I was wondering why the frisbee was getting bigger,
-                then it hit me.
+                {data.joke.content}
             </p>
+            <Link to={(data.joke.id).toString()}>
+                "{data.joke.name}" Permalink
+            </Link>
         </div>
     )
 }
